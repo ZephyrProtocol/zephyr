d9be7d0381a65b80ca5ae11dbe68e96960a192fa
ZephyrProtocol <zephyrcurrencyprotocol@gmail.com>
2023-07-12 10:48:06 +1200

    Remove legacy monero block hash patch

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -981,7 +981,6 @@ namespace cryptonote
   {
     for (const auto &o: tx.vout)
     {
-      // from v15, require outputs have view tags
       CHECK_AND_ASSERT_MES(o.target.type() == typeid(txout_zephyr_tagged_key), false, "wrong variant type: "
         << o.target.type().name() << ", expected txout_zephyr_tagged_key in transaction id=" << get_transaction_hash(tx));
     }
@@ -1462,36 +1461,6 @@ namespace cryptonote
     }
 
     bool hash_result = get_object_hash(get_block_hashing_blob(b), res);
-    if (!hash_result)
-      return false;
-
-    if (b.miner_tx.vin.size() == 1 && b.miner_tx.vin[0].type() == typeid(cryptonote::txin_gen))
-    {
-      const cryptonote::txin_gen &txin_gen = boost::get<cryptonote::txin_gen>(b.miner_tx.vin[0]);
-      if (txin_gen.height != 202612)
-        return true;
-    }
-
-    // EXCEPTION FOR BLOCK 202612
-    const std::string correct_blob_hash_202612 = "3a8a2b3a29b50fc86ff73dd087ea43c6f0d6b8f936c849194d5c84c737903966";
-    const std::string existing_block_id_202612 = "bbd604d2ba11ba27935e006ed39c9bfdd99b76bf4a50654bc1e1e61217962698";
-    crypto::hash block_blob_hash = get_blob_hash(*blob);
-
-    if (string_tools::pod_to_hex(block_blob_hash) == correct_blob_hash_202612)
-    {
-      string_tools::hex_to_pod(existing_block_id_202612, res);
-      return true;
-    }
-
-    {
-      // make sure that we aren't looking at a block with the 202612 block id but not the correct blobdata
-      if (string_tools::pod_to_hex(res) == existing_block_id_202612)
-      {
-        LOG_ERROR("Block with block id for 202612 but incorrect block blob hash found!");
-        res = null_hash;
-        return false;
-      }
-    }
     return hash_result;
   }
   //---------------------------------------------------------------
